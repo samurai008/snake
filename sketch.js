17b2f1fd05954014255d81435f65f0216a2b1097
Nilabjo Sanyal <n.sanyal@hotmail.com>
2018-09-14 07:10:14 +0530

    snake v0.1

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let s;
+let rez = 10;
+let food;
+let w;
+let h;
+
+function setup () {
+    createCanvas(400, 400);
+    w = floor(width / rez);
+    h = floor(height / rez);
+    s = new snake();
+    foodLocation();
+    frameRate(5);
+}
+
+function foodLocation() {
+    let x = floor(random(w));
+    let y = floor(random(h));
+    food = createVector(x, y);
+}
+
+function keyPressed() {
+    console.log(keyCode);
+    switch(keyCode) {
+        case LEFT_ARROW:
+            s.setDir(-1, 0);
+        break;
+        case RIGHT_ARROW:
+            s.setDir(1, 0);
+        break;
+        case DOWN_ARROW:
+            s.setDir(0, 1);
+        break;
+        case UP_ARROW:
+            s.setDir(0, -1);
+        break;
+    }
+}
+
+function draw() {
+    scale(rez);
+    background(220);
+    s.update();
+    s.show();
+
+    noStroke();
+    fill(255, 0, 0);
+    rect(food.x, food.y, 1, 1);
+}
